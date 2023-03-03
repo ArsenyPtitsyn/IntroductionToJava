886992c9ed3304b53dc31311ff66538c06e92c97
Arseny Ptitsyn <senyabird@yandex.ru>
2023-03-03 20:56:49 +0300

    Removed concatenation from StringBuilder.

--- src/homework1/RevertWords.java
+++ src/homework1/RevertWords.java
@@ -4,6 +4,10 @@ public class RevertWords {
     private static final String exampleString = "Let it be! Let it be! Let it be! Let it be! whisper words of wisdom let it be";
 
     public static void main(String[] args) {
+        System.out.println(reverseString());
+    }
+
+    private static StringBuilder reverseString() {
         String[] wordsFromExampleString = exampleString.split(" ");
 
         StringBuilder resultString = new StringBuilder();
@@ -11,6 +15,6 @@ public class RevertWords {
         for (int i = 0; i < numberOfWords; i++) {
             resultString.append(wordsFromExampleString[numberOfWords - 1 - i]).append(" ");
         }
-        System.out.println(resultString);
+        return resultString;
     }
 }
