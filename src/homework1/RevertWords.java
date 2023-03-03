a1812df2864655104a8d2a869dcd26d91cad6e38
Arseny Ptitsyn <senyabird@yandex.ru>
2023-03-03 22:16:28 +0300

    Added argument in method reverseString(). Did the Task2.

--- src/homework1/RevertWords.java
+++ src/homework1/RevertWords.java
@@ -1,14 +1,14 @@
 package homework1;
 
 public class RevertWords {
-    private static final String exampleString = "Let it be! Let it be! Let it be! Let it be! whisper words of wisdom let it be";
+    private static final String EXAMPLE_STRING = "Let it be! Let it be! Let it be! Let it be! whisper words of wisdom let it be";
 
     public static void main(String[] args) {
-        System.out.println(reverseString());
+        System.out.println(reverseString(EXAMPLE_STRING));
     }
 
-    private static StringBuilder reverseString() {
-        String[] wordsFromExampleString = exampleString.split(" ");
+    private static StringBuilder reverseString(String s) {
+        String[] wordsFromExampleString = s.split(" ");
 
         StringBuilder resultString = new StringBuilder();
         int numberOfWords = wordsFromExampleString.length;
--- src/homework1/Task2.java
+++ src/homework1/Task2.java
@@ -1,5 +1,25 @@
+/*
+  Дана последовательность целых чисел, оканчивающаяся нулем. Найти сумму положительных чисел,
+  после которых следует отрицательное число.
+*/
+
 package homework1;
 
 public class Task2 {
 
+    public static void main(String[] args) {
+        int[] array = {1,-5,-6,8,3,6,-5,3,-132,4};
+        int sumOfPositivesBehindNegative = calculateSum(array);
+        System.out.println(sumOfPositivesBehindNegative);
+    }
+
+    private static int calculateSum(int[] array) {
+        int sum = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < 0)
+                if (array[i - 1] > 0)
+                    sum += array[i - 1];
+        }
+        return sum;
+    }
 }
