e5455efc42792e86093b1aa160288ede04ad8714
Arseny Ptitsyn <senyabird@yandex.ru>
2023-03-04 14:26:51 +0300

    Sum of binary strings is done. Parse way.

---
+++ src/homework1/BinarySumParseWay.java
@@ -0,0 +1,16 @@
+package homework1;
+
+public class BinarySumParseWay {
+
+    public static void main(String[] args) {
+        String a =   "11000111010101110";
+        String b = "1000001111100111011";
+        System.out.println(binarySum(a, b));
+    }
+
+    private static String binarySum(String a, String b) {
+        int firstNumber = Integer.parseInt(a, 2);
+        int secondNumber = Integer.parseInt(b, 2);
+        return Integer.toBinaryString(firstNumber + secondNumber);
+    }
+}
